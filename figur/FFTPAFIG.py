83b69bf38dfb0482f22cb938d67d332ff646dc5d
nikla <nikla@DESKTOP-3UAG37R.local>
2020-04-03 19:54:17 +0200

    Tilføjelse af mappe "lyd" heriblandt flere lydfiler og stft af signal

--- figur/FFTPAFIG.py
+++ figur/FFTPAFIG.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 from matplotlib import rcParams
 rcParams.update({'figure.autolayout': True})
 
-rate, audio = read("piano-C4.wav")
+rate, audio = read("../lyd/piano-C4.wav")
+rateT, audioT = read("../lyd/trumpet-C4.wav")
 
 fourier = np.fft.fft(audio)
 fourier = np.abs(fourier)
@@ -26,24 +27,65 @@ plt.savefig("C4audio.pdf")
 
 # window=['rectangular','hanning','hamming','blackman']
 
-def stftplot(window,windowlength,overlap,inputsignal):
-    for i in window:
-        f,t,Zxx = stft(inputsignal, fs=rate, window=i, nfft=len(audio) ,nperseg=windowlength, noverlap=windowlength*overlap, boundary='zeros',axis=-1)     
+def stftplot(window,windowlength,overlap,inputsignal,samplerate, plot=True):
+    """
+    GENERAL
+    ----------
+    This function takes an input signal and plots a spectrogram of windowed signal.
+    The function uses the function stft from scipy.signal.
+    
+    
+    Parameters
+    ----------
+    window : TYPE str
+        DESCRIPTION: Specifies the applied window function. 
+                     If window function is not specified, default: Hanning window
+               
+    windowlength : TYPE int
+        DESCRIPTION: Length of each frame
+                     Defaults to: 256
+            
+    overlap : TYPE: float, int
+        DESCRIPTION: Percentwise overlap from frame to frame
+                     Default: 0.5
+            
+    inputsignal : TYPE list, tuple, array
+        DESCRIPTION: The signal which is analysed
+            
+    samplerate : TYPE float, int
+        DESCRIPTION: samples per second
+    
+    plot :TYPE bool
+        DESCRIPTION: When True, a spectrogram of winodwed 
+                     inputsignal is plotted. 
+                     Default: True
+
+    Returns
+    -------
+    f : TYPE Array of float 64
+        DESCRIPTION: Array of sample frequencies.
+            
+    t : TYPE Array of float 64
+        DESCRIPTION: Array of segment times.
+            
+    Zxx : TYPE Array of complex64
+        DESCRIPTION: STFT of windowed signal          
+    """
+    
+    f,t,Zxx = stft(inputsignal, fs=samplerate, window=window, nfft=windowlength ,nperseg=windowlength, noverlap=windowlength*overlap)     
+    
+    if plot==True:    
         plt.figure()
         plt.pcolormesh(t, f, np.abs(Zxx), vmin=0,)
-        plt.title(f'STFT Magnitude, window: {i:}')
+        plt.colorbar()
+        plt.title(f'STFT Magnitude, window: {window:}')
         plt.ylabel('Frequency [Hz]')
         plt.xlabel('Time [sec]')
-
         plt.savefig(f'spectrogramC4{window}.jpg', dpi=150)
     return f, t, Zxx
 
 overlap= 0.5
-f,t,Zxx=stftplot(['hanning'],200,overlap,audio)
+f,t,Zxx=stftplot('hanning',200,overlap,audio,rate)
+#Zxx= np.reshape(Zxx, (len(Zxx[0]),len(Zxx)))
 
-plt.figure()
-plt.plot(f[5:4000], fourier[5:4000]) 
-
-print(Zxx[0]) #Zxx[0] består af frekvensindhold til frame 0
-
-print((time[-1])/(1/rate*200*(1-overlap))) #Sådan beregnes antal af frames
+#print((time[-1])/(1/rate*200*(1-overlap))) #Sådan beregnes antal af frames
---
+++ lyd/STFTsignal.py
@@ -0,0 +1,120 @@
+from scipy.io.wavfile import read
+from scipy.signal import stft
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+rcParams.update({'figure.autolayout': True})
+
+
+def audiosignal(audiofile, info=None):
+    samplingrate,audio=read(audiofile)
+    
+    if info==True:
+        print(f'''
+          Input info:
+          Length of input signal: {len(audio)/samplingrate:.0f} seconds
+          Amount of samples in total: {len(audio):.0f} samples
+          Amount of samples within 10 sec: {10*samplingrate} samples
+          Number of sound channels: {np.size(audio[0])}
+_____________________________________________________________________________
+          ''')
+    
+    amount_samples_total = len(audio)
+    amount_samples_10s   = 10*samplingrate
+    nr_channels = np.size(audio[0])
+    
+    if amount_samples_total > amount_samples_10s and nr_channels == 1:
+        audio= audio[0:10*samplingrate]
+        print('Inputsignal is shortened to samples within 10 s')
+    elif amount_samples_total > amount_samples_10s and nr_channels != 1:
+        audio= audio[0:10*samplingrate:,0]
+        print('Inputsignal is shortened to samples within 10 s and sound channel 1 is chosen')
+    else:
+        audio
+    
+
+    return samplingrate, audio
+
+def FourierAndtimePLOTS(inputsignal, samplingrate):
+    endpoint_of_fftplot = int(len(inputsignal)/2)
+    
+    fourier = np.fft.fft(inputsignal)
+    fourier = np.abs(fourier)
+
+    freq = np.fft.fftfreq(len(fourier), d=1/samplingrate)
+    
+    plt.figure()
+    plt.plot(freq[0:endpoint_of_fftplot], fourier[0:endpoint_of_fftplot])
+    plt.xlabel("Frequency [Hz]")
+    plt.ylabel("Magnitude")
+    #plt.savefig(f"C4freq.pdf")
+
+    time = np.linspace(0, len(inputsignal)/samplingrate, len(inputsignal))
+    plt.figure()
+    plt.plot(time, inputsignal, 'r')
+    plt.xlabel("Time [s]")
+    #plt.savefig("C4audio.pdf")
+
+
+def stft_of_signal(window,windowlength,overlap,inputsignal,samplerate, plot=None):
+    """
+    GENERAL
+    ----------
+    This function takes an input signal and plots a spectrogram of windowed signal.
+    The function uses the function stft from scipy.signal.
+    
+    
+    Parameters
+    ----------
+    window : TYPE str
+        DESCRIPTION: Specifies the applied window function. 
+                     If window function is not specified, default: Hanning window
+               
+    windowlength : TYPE int
+        DESCRIPTION: Length of each frame
+                     Defaults to: 256
+            
+    overlap : TYPE: float, int
+        DESCRIPTION: Percentwise overlap from frame to frame
+                     Default: 0.5
+            
+    inputsignal : TYPE list, tuple, array
+        DESCRIPTION: The signal which is analysed
+            
+    samplerate : TYPE float, int
+        DESCRIPTION: samples per second
+    
+    plot : bool, Optional
+        DESCRIPTION: When True, spectrogram of windowed input signal is plotted
+                     Default: None
+
+    Returns
+    -------
+    f : TYPE Array of float 64
+        DESCRIPTION: Array of sample frequencies.
+            
+    t : TYPE Array of float 64
+        DESCRIPTION: Array of segment times.
+            
+    Zxx : TYPE Array of complex64
+        DESCRIPTION: STFT of windowed signal          
+    """
+    f,t,Zxx = stft(inputsignal, fs=samplerate, window=window, nfft=windowlength ,nperseg=windowlength)     
+    
+    
+    if plot==True:
+        plt.figure()
+        plt.pcolormesh(t, f, np.abs(Zxx), vmin=0)
+        plt.colorbar()
+        plt.title(f'Spectrogram of input signal windowed with {window:} winow')
+        plt.ylabel('Frequency [Hz]')
+        plt.xlabel('Time [sec]')
+    return f, t, Zxx
+      
+#Liste der indeholder lydfilerne
+audiofile =("piano-C4.wav", "trumpet-C4.wav", "BandofHorses.wav", "KingsOfMetal.wav")
+
+rate, audio = audiosignal(audiofile[0], info=True)
+
+FourierAndtimePLOTS(audio, rate)
+f, t, Zxx = stft_of_signal('hanning',2**13,0.5,audio,rate,plot=True)
