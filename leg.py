7447ee68dfc9b3a894047123ff6f6bb088902c74
Dana <vu@outlook.dk>
2020-05-07 12:30:59 +0200

    Update leg.py

--- leg.py
+++ leg.py
@@ -5,18 +5,29 @@ import numpy as np
 from numpy import linalg as LA
 import matplotlib.pyplot as plt
 
+database = []
 
-# Open a file
-path = "lyd/"
-dirs = os.listdir( path )
+def loadDatabase ():
+    # Open a file
+    path = "lyd/"
+    dirs = os.listdir( path )
+    
+    # This would print all the files and directories
+    for file in dirs:
+       if file == ".DS_Store": #Mac file
+           continue
+       songdict = {}
+       print (file)
+       samplingrate, audio = STFTsignal.audiosignal(path + file)
+       _, _, Zxx = STFTsignal.stft_of_signal('hanning', 4096, 0.5, audio, samplingrate)
+       songdict["mean"], songdict["eigen"], songdict["weights"] = PCA(Zxx)
+       songdict["name"] = file
+       database.append (songdict)
+       
+loadDatabase ()
+print (database)       
 
-# This would print all the files and directories
-for file in dirs:
-   if file == ".DS_Store":
-       continue
-   print (file)
-   samplingrate, audio = STFTsignal.audiosignal(path + file)
-   _, _, Zxx = STFTsignal.stft_of_signal('hanning', 4096, 0.5, audio, samplingrate)
-   mean, eigen, weights = PCA(Zxx)
-
+def loadTestSong (filename):
+    samplingrate, audio = STFTsignal.audiosignal(filename)
+    _, _, Zxx = STFTsignal.stft_of_signal('hanning', 4096, 0.5, audio, samplingrate)
 
