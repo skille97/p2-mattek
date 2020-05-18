a7f7dae61e049e9bd86a49a966f8c26860ebd884
nikla <nikla@DESKTOP-3UAG37R.local>
2020-05-18 15:32:32 +0200

    Sidste oprydning

--- noise_addition.py
+++ noise_addition.py
@@ -1,32 +1,10 @@
 import STFTsignal
 from SignalToNoiseRatio import addNoise_and_STFT
 from scipy.io.wavfile import write
-import matplotlib.pyplot as plt
-from numpy import linalg as LA
-import os
-from p2PCA import PCA
 import numpy as np
-
-database = []
-testSong = {}
-
-def loadDatabase ():
-    # Open a file
-    path = "lyd/"
-    dirs = os.listdir( path )
-    
-    # This would print all the files and directories
-    for file in dirs:
-       if file == ".DS_Store": #Mac file
-           continue
-       songdict = {}
-       print (file)
-       samplingrate, audio = STFTsignal.audiosignal(path + file)
-       _, _, Zxx = STFTsignal.stft_of_signal(audio, samplingrate)
-       songdict["mean"], songdict["eigen"], songdict["weights"] = PCA(Zxx)
-       songdict["name"] = file
-       database.append (songdict)
-def PCA_and_SNR(audiofile, noiceRange="range(0,max(audio)*2, int(max(audio)*2/100))"):
+from recognition import recognition, loadDatabase
+      
+def PCA_and_SNR(audiofile, noiceRange="range(0,max(audio)*2, int(max(audio)*2/1000))"):
     """
     Noise is added until the audiofile can not be recognised anymore by algorithm.
     When the audiofile is not recognised anymore, an .wav file is created with the
@@ -38,44 +16,38 @@ def PCA_and_SNR(audiofile, noiceRange="range(0,max(audio)*2, int(max(audio)*2/10
 
     noiseRange : TYPE, optional, str
         DESCRIPTION:  range of added noise
-        The default is "range(0,max(audio)*2, int(max(audio)*2/100))".
+        The default is "range(0,max(audio)*2, int(max(audio)*2/1000))".
     
     Returns
     -------
     A file with noise added is created ("noisesong.wav")
 
     """
+    testSong = {}
     ZxxClean = STFTsignal.getSTFTofFile(audiofile)
     samplingrate, audio = STFTsignal.audiosignal(audiofile)
-    
+    SNR = np.var(ZxxClean)/np.var(addNoise_and_STFT(audio, samplingrate, 0)[0])
     for i in eval(noiceRange):
         
-        testSong["spektrogram"], noisyAudio = addNoise_and_STFT(audio, samplingrate, i)
+        testSong["spectrogram"], noisyAudio = addNoise_and_STFT(audio, samplingrate, i)
         testSong["name"] = audiofile
+        
+        prevSNR = SNR
         SNR = np.var(ZxxClean)/np.var(addNoise_and_STFT(audio, samplingrate, i)[0])
-        recon_song = recognition()
-        print(f"SNR value: {SNR}")
-        if f'lyd/{recon_song}' != audiofile:
+        
+        recon_song = recognition(database, testSong, plot=None, info=None)
+        if f'lyd/{recon_song}' != audiofile or SNR<0.02:
+            print(f"""
+Actual song: {audiofile}
+SNR (%): {prevSNR*100:0.4}
+                  """)
             noisyAudio = np.asarray(noisyAudio, dtype=np.int16)
             write('noisesong.wav', samplingrate, noisyAudio)
             break
 
-def recognition():
-    for i in range(len(database)):
-        scores = []
-        for j in range(testSong["spektrogram"].shape[1]):
-            u = np.transpose(database[i]["eigen"]) @ (testSong["spektrogram"][:, j] - database[i]["mean"])
-            scores.append(LA.norm(u - database[i]["weights"][:, j]))
-        database[i]["score"] = sum(scores)/len(scores)
-
-    lowsScore = database[0]
-    for song in database[1:]:
-        if lowsScore["score"] > song["score"]:
-            lowsScore = song
-    print("         ")
-    print(f"The song with the lowest score is {lowsScore['name']} with a score of {lowsScore['score']}")
-    return lowsScore['name']
-
-if __name__ == '__main__':    
-    loadDatabase()
-    PCA_and_SNR("lyd/Band of Horses - The Funeral.wav")
+if __name__ == '__main__':
+    # if main, all the songs in database is analysed (Takes time)
+    path = 'lyd/'
+    database = loadDatabase()
+    for i in database:
+        PCA_and_SNR(path + i["name"])
--- recognition.py
+++ recognition.py
@@ -1,57 +1,96 @@
-import os, sys
+import os
 from p2PCA import PCA
 import STFTsignal
 import numpy as np
 from numpy import linalg as LA
 import matplotlib.pyplot as plt
 
-database = []
-testSong = {}
 
 def loadDatabase ():
+    """
+    Database is created and information stored in a list of dictionaries
+
+    Returns
+    -------
+    database : list of dictionaries with the neccessary information
+
+    """
+    database = []
     # Open a file
     path = "lyd/"
     dirs = os.listdir( path )
     
-    # This would print all the files and directories
+    # This prints all of the files and directories
     for file in dirs:
        if file == ".DS_Store": #Mac file
            continue
        songdict = {}
        print (file)
-       samplingrate, audio = STFTsignal.audiosignal(path + file)
-       _, _, Zxx = STFTsignal.stft_of_signal(audio, samplingrate)
+       Zxx = STFTsignal.getSTFTofFile(path + file) #STFT of the file
+       #mean, eigen and weights are stored in dictionary songdict
        songdict["mean"], songdict["eigen"], songdict["weights"] = PCA(Zxx)
        songdict["name"] = file
-       database.append (songdict)
-       
+       database.append (songdict)  
+    return database
 
-def loadTestSong (filename, noice):
-    samplingrate, audio = STFTsignal.audiosignal(filename)
-    audio = audio + np.random.randn(audio.size) * noice
-    _, _, testSong["spektrogram"] = STFTsignal.stft_of_signal(audio, samplingrate)
+def loadTestSong (filename):
+    """
+    filename and STFT of the file stored in a dictionary
+    
+    Parameters
+    ----------
+    filename : TYPE .wav file
+    
+    Returns
+    -------
+    testSong : dictioary with information of filename and its STFT 
+    """
+    testSong = {}
+    #information of analysed song stored in dictionary testSong
+    testSong["spectrogram"] = STFTsignal.getSTFTofFile(filename)
     testSong["name"] = filename
+    return testSong
 
-def recognition():
+def recognition(database, testSong, plot=True, info=True):
+    """
+    An average score is calculated for every song in database with respect to testsong
+    and the most alike song from database is printed
+    Parameters
+    ----------
+    database : database with songs, returned from loadDatabase function
+
+    testSong : TYPE dictionary with keywords "spectrogram" and "name"
+    
+    plot : TYPE, optional
+        DESCRIPTION: Plots the score
+                     The default is True.
+    """
+    #Score for every song in database with respect to testsong
     for i in range(len(database)):
         scores = []
-        for j in range(testSong["spektrogram"].shape[1]):
-            u = np.transpose(database[i]["eigen"]) @ (testSong["spektrogram"][:, j] - database[i]["mean"])
+        for j in range(testSong["spectrogram"].shape[1]): #every frame of STFT
+            #Score is calculated
+            u = np.transpose(database[i]["eigen"]) @ (testSong["spectrogram"][:, j] - database[i]["mean"])
             scores.append(LA.norm(u - database[i]["weights"][:, j]))
+        #mean value of the scores
         database[i]["score"] = sum(scores)/len(scores)
-        print(f"{database[i]['name']} has a score of {database[i]['score']}")
-        plt.plot(scores)
-        plt.title(database[i]['name'])
-        plt.show()
+        if plot == True:
+            print(f"{database[i]['name']} has a score of {database[i]['score']}")
+            plt.plot(scores)
+            plt.title(database[i]['name'])
+            plt.show()
 
+    #The lowest score is found and is the most alike to testSong
     lowsScore = database[0]
     for song in database[1:]:
         if lowsScore["score"] > song["score"]:
             lowsScore = song
-    print("         ")
-    print(f"The song with the lowest score is {lowsScore['name']} with a score of {lowsScore['score']}")
+    if info == True:        
+        print("         ")
+        print(f"The song with the lowest score is {lowsScore['name']} with a score of {lowsScore['score']}")
+    return lowsScore['name']
 
 if __name__ == '__main__':
-    loadDatabase()
-    loadTestSong("optagelser/band of horses (optag).wav", 0)
-    recognition()
+    database = loadDatabase()
+    testSong = loadTestSong("optagelser/band of horses (optag).wav")
+    recognition(database, testSong)
